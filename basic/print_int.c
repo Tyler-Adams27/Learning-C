c5c00bdde7b09de93beda9de1dfe67e7627dbf23
= <tyler.adams.prof@gmail.com>
2025-07-08 19:10:05 +0100

    Inital commit

---
+++ basic/print_int.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+  int number;
+  printf("Enter a number!\n");
+  scanf("%i", &number);
+  printf("The number you entered was %i!\n", number);
+  return 0;
+}
---
+++ basic/print_name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main(){
+  char* name;
+  printf("Hello! What is your name?\n");
+  scanf("%s", name);
+  printf("Hello, %s!\n", name);
+  return 0;
+}
