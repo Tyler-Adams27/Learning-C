95fbb086ca6330d619b436efceb6fde6a0f1e81c
= <tyler.adams.prof@gmail.com>
2025-07-08 19:36:51 +0100

    added add_two_numbers.c

---
+++ basic/add_two_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+  
+  int first_num;
+  int second_num;
+  float answer;
+
+  printf("Enter the first number.\n");
+  scanf("%i", &first_num);
+  printf("Enter the second number.\n");
+  scanf("%i", &second_num);
+  answer = first_num + second_num;
+  printf("The answer is %f.\n", answer);
+  return 0;
+}
+
+
+
+
+
+
+
+
+
